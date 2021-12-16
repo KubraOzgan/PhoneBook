8cd996aa7da8123d422a55c65723dcc0c0d8f35e
Kübra Özgan <86002359+KubraOzgan@users.noreply.github.com>
2021-12-17 00:27:26 +0300

    Update validate.js

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -3,11 +3,11 @@ const httpStatus = require("http-status");
 const validate = (schema) => (req, res, next) => {
     const {value, error}= schema.validate(req.body); //value req ile gelen degerin kendisi
     if(error) {
-        //error.details = [{message: ''}, {message: ''}, ...] default olarak errorlarin hepsini boyle verir
-        const errorMessage = error.details?.map(detail => detail.message).join(', ') //detail obj icinden sadece mesajlari al, sadece mesajlarin oldugu array dondur
-        //['','',....] => '', '', ... join ile String haline getirdik
+        //error.details = [{message: ''}, {message: ''}, ...] default 
+        const errorMessage = error.details?.map(detail => detail.message).join(', ') 
+        //['','',....] => '', '', ... 
         res.status(httpStatus.BAD_REQUEST).json({ error : errorMessage } );
-        return; //else yerine? 35.25
+        return;
     }
     Object.assign(req, value); //validate ten çikan data ile reqi bind et ve dişari gonder
     return next();
